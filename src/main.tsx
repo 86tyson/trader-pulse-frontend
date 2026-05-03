8f292ecb3db9dda2061ef23e3008bf481300bf79
86tyson <chris@tysoninsulation.com>
2026-05-03 09:31:38 -0700

    frontend: remove legacy PasswordGate (single-login flow)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { PasswordGate } from "./PasswordGate.tsx";
 
 document.documentElement.classList.add("dark");
 
-createRoot(document.getElementById("root")!).render(
-  <PasswordGate>
-    <App />
-  </PasswordGate>
-);
+createRoot(document.getElementById("root")!).render(<App />);
